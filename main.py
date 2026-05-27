c9b3b79e051025b15b696d23194c3580f7e59e66
gilberto <gilbertolima3004@gmail.com>
2026-05-27 13:16:52 -0300

    Corrigindo código fonte da API Livros

--- main.py
+++ main.py
@@ -1,47 +1,68 @@
 
+# API de Livros
 
-from fastapi import FastAPI, HTTPException  # type: ignore
+# Métodos HTTP: GET, POST, PUT, DELETE
+
+# POST - Adicionar novos livros (Create)
+# GET - Buscar os dados dos livros (Read)
+# PUT - Atualizar informações dos livros (Update)
+# DELETE - Deletar informações dos livros (Delete)
+
+# CRUD
+
+# Create
+# Read
+# Update
+# Delete
+
+# Comando para inicializar servidor: fastapi dev .\Aula1.py
+
+from fastapi import FastAPI, HTTPException # type: ignore
 
 app = FastAPI()
 
-tarefas = [
-    {"nome":"Tarefa1", "descricao":"Lavar louça", "concluida": False},
-    {"nome":"Tarefa 2", "descricao":"Estudar Python", "concluida": False},    
-    {"nome":"Tarefa 3", "descricao":"2hrs de games para curtição", "concluida": False}
-]
+livros = {}
 
-@app.get("/tarefas")
-def get_tarefas():
-    if not tarefas:
-        return {"message": "Não existe essa tarefa."}
+@app.get("/livros")
+def get_livros():
+    if not livros:
+        return {"message": "Não existe nenhum livro"} 
     else:
-        return {"tarefas": tarefas}
+        return {"livros": livros} 
+
+# id do livro
+# nome do livro
+# autor do livro
+# ano de lançamento do livro
+
+@app.post("/adicionar")
+def post_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+    if id_livro in livros:
+        raise HTTPException(status_code=400, detail="Esse livro já existe!")
+    else:
+        livros[id_livro] = {"nome_livro": nome_livro, "autor_livro": autor_livro, "ano_livro": ano_livro}
+        return {"message": "O livro foi criado com sucesso"}
     
-@app.post("/adicionar/{nome}/{descricao}")
-def post_tarefas(nome: str, descricao: str):
-    for tarefa in tarefas:
-        if tarefa["nome"] == nome:
-            raise HTTPException(status_code = 400, detail = "Essa tarefa já existe!")
-        
-    nova_tarefa = {"nome": nome, "descricao": descricao, "concluida": False}    
-    tarefas.append(nova_tarefa)
+@app.put("/atualizar/{id_livro}")
+def put_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+    livro = livros.get(id_livro)
+    if not livro:
+        raise HTTPException(status_code=404, detail="Esse livro não foi encontrado!")
+    else:
+        if nome_livro:
+            livro["nome_livro"] = nome_livro
+        if autor_livro: 
+            livro["autor_livro"] = autor_livro
+        if ano_livro:
+            livro["ano_livro"] = ano_livro
 
-    return{"message": "A tarefa foi adicionada com sucesso!"}
+        return {"message": "As informações do seu livro foram atualizados com sucesso!"}
+    
+@app.delete("/deletar/{id_livro}")
+def delete_livro(id_livro: int):
+    if id_livro not in livros:
+        raise HTTPException(status_code=404, detail="Esse livro não foi encontrado!")
+    else:
+        del livros[id_livro]
 
-@app.put("/atualizar/{nome}")
-def put_tarefa(nome: str):
-    for tarefa in tarefas:
-        if tarefa["nome"] == nome:
-            tarefa["concluida"] = True
-            return{"message": "A tarefa foi alterada com sucesso!"}
-        
-    raise HTTPException(status_code = 400, detail = "Essa tarefa não existe para ser atualizada!")    
-
-@app.delete("/deletar/{nome}")
-def delete_tarefa(nome: str):
-    for tarefa in tarefas:
-        if tarefa["nome"] == nome:
-            tarefas.remove(tarefa)
-            return {"message": "Tarefa deletada com sucesso!"}    
-        
-    raise HTTPException(status_code=404, detail="Não existe tarefa para ser excluída!")
+        return {"message": "Seu livro foi deletado com sucesso!"}
